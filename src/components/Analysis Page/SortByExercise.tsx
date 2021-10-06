17c6e93d7e6f018616628970d2778df96a15c35c
StanHus <guseletovS@gmail.com>
2021-10-06 15:55:44 +0100

    progress evaluation

--- src/components/Analysis Page/SortByExercise.tsx
+++ src/components/Analysis Page/SortByExercise.tsx
@@ -110,7 +110,7 @@ const chartData = {
 
   const pulledAnalysis = analysis.filter((val: IAnalysis) => (val.exercise_name === exercise)).map((val: IAnalysis) => (<div><p>You trained {val.exercise_name} {val.days_trained} {(val.days_trained > 1) ? "times" : "time"}. The average weight per rep per session was {Math.floor(val.average_total_weight)} kgs. This means next time you ideally need to do {Math.ceil(val.average_sets)} sets, with {Math.ceil(val.average_reps)} reps each, with a weight of at least {increaseWeight(val.average_weight/(val.average_reps*val.average_sets))} kgs. 
   </p>
-  <p>Since you started, you have increased your total weight in this exercise by a factor of {(val.max_session/val.min_session).toFixed(2)}. That's a {(val.max_session/val.min_session) >1.5 ? "great" : "solid"} job!</p>
+  <p>Since you started, you have increased your total weight in this exercise by a factor of {(val.max_session/val.min_session).toFixed(2)}. That's a {((val.max_session/val.min_session) <1.4 && (val.max_session/val.min_session) > 1) ? "fair" : (val.max_session/val.min_session) === 1 ? "beginners" : "great"} progress!</p>
   <p>Your min weight per session was {val.min_session}. If you are feeling tired, do at least {val.average_sets} sets of {val.average_reps} reps with {increaseWeight(val.min_session/(val.average_reps*val.average_sets))} kgs</p>
   <p>Your max weight per session was {val.max_session} - to top that you will need to do {val.average_sets} sets of {val.average_reps} reps with {increaseWeight(val.max_session/(val.average_reps*val.average_sets))} kgs. Are you up to the task?</p>
   </div>))
