cf73dfbd329deb467a851f667d5a8426b57028ef
StanHus <guseletovS@gmail.com>
2021-10-19 12:32:38 +0100

    creates users np

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,100 @@
 import Header from "./components/Main Body/Header";
-import Footer from "./components/Main Body/Footer"
+import Footer from "./components/Main Body/Footer";
 import InputSession from "./components/Main Body/InputSession";
 import ListSessions from "./components/Main Body/ListSessions";
 import SuggestionBox from "./components/Main Body/Suggestions";
 import ListProgress from "./components/ProgressPage/ProgressList";
 import Analysis from "./components/Analysis Page/AnalysisMain";
-// import SignUp from "./components/users/CreateUser";
-import "./css/style.css"
+import SignUp from "./components/users/CreateUser";
+import "./css/style.css";
 import { useState } from "react";
-import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
-
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import LoginSection from "./components/users/LoginSection";
 
 function App() {
-  const [state, setState] = useState(true)
+  const [state, setState] = useState(true);
 
-    return(
-        <>
-         <BrowserRouter>
-            <Header />
-            <nav>
-                   {state && 
-                        <button className="switchButton">
-                        <Link onClick={() => setState(false)} className = "navlink" to="/progress">Exercises</Link>
-                    </button>}
-                    {!state &&             
-                    <button className="switchButton">                        
-                        <Link onClick={() => setState(true)} className = "navlink" to="/">Main Page</Link>
-                    </button>}
-                    {!state &&             
-                    <button className="switchButton">                        
-                        <Link onClick={() => setState(true)} className = "navlink" to="/analysis">Analysis</Link>
-                    </button>}
-                    {/* <button className="switchButton"> 
-                        <Link className = "navlink" to="/userPage">UsersPage</Link>
-                    </button>                            */}
-            </nav>
-            <main>
-                <Switch>
-                    <Route exact path = "/">
-                        <section>
-                         <body className="body">
-                         <div className = "input">
-                          <InputSession />
-                           </div>
-                          <SuggestionBox />
-                          <ListSessions />
-                       </body>
-                       <Footer />
-                      </section> 
-                    </Route>
-                    <Route exact path = "/progress">
-                        <section>
-                            <ListProgress />
-                            <Footer />
-                        </section>
-                    </Route>
-                    <Route exact path = "/analysis">
-                        <section>
-                            <Analysis />
-                            <Footer />
-                        </section>
-                    </Route>
-                    {/* <Route exact path = "/userPage">
-                        <section>
-                            <SignUp />
-                        </section>
-                    </Route> */}
-                </Switch>
-            </main>
-         </BrowserRouter>
-        </>
-    )
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <nav>
+          {state && (
+            <button className="switchButton">
+              <Link
+                onClick={() => setState(false)}
+                className="navlink"
+                to="/progress"
+              >
+                Exercises
+              </Link>
+            </button>
+          )}
+          {!state && (
+            <button className="switchButton">
+              <Link onClick={() => setState(true)} className="navlink" to="/">
+                Main Page
+              </Link>
+            </button>
+          )}
+          {!state && (
+            <button className="switchButton">
+              <Link
+                onClick={() => setState(true)}
+                className="navlink"
+                to="/analysis"
+              >
+                Analysis
+              </Link>
+            </button>
+          )}
+          <button className="switchButton">
+            <Link className="navlink" to="/signUp">
+              UsersPage
+            </Link>
+          </button>
+        </nav>
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <section>
+                <body className="body">
+                  <div className="input">
+                    <InputSession />
+                  </div>
+                  <SuggestionBox />
+                  <ListSessions />
+                </body>
+                <Footer />
+              </section>
+            </Route>
+            <Route exact path="/progress">
+              <section>
+                <ListProgress />
+                <Footer />
+              </section>
+            </Route>
+            <Route exact path="/analysis">
+              <section>
+                <Analysis />
+                <Footer />
+              </section>
+            </Route>
+            <Route exact path="/login">
+              <section>
+                <LoginSection />
+              </section>
+            </Route>
+            <Route exact path="/signUp">
+              <section>
+                <SignUp />
+              </section>
+            </Route>
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/users/CreateUser.tsx
+++ src/components/users/CreateUser.tsx
@@ -1,70 +1,75 @@
-import { useState } from "react"
-import "../../css/style.css"
-import LoginPage from "./LogMeIn"
+import { useState } from "react";
+import "../../css/style.css";
+import LoginPage from "./LogMeIn";
 
+export default function SignUp() {
+  const [userName, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [completed, setCompleted] = useState(false);
 
-export default function SignUp (){
-
-    const [userName, setUsername] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [completed, setCompleted] = useState(false)
-
-    const onSubmitForm = async (e: any) => {
-        setCompleted(true)
-            e.preventDefault();
-        try {
-            await fetch("https://mysterious-reaches-13528.herokuapp.com/users", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                userName: userName,
-                email: email,
-                password: password
-              }),
-          });
-        } catch (err) {
-          console.error(err);
-        }
+  const onSubmitForm = async (e: any) => {
+    if (userName !== "" && email !== "" && password !== "") {
+      e.preventDefault();
+      try {
+        await fetch("https://mysterious-reaches-13528.herokuapp.com/users", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            userName: userName,
+            email: email,
+            password: password,
+          }),
+        });
+      } catch (err) {
+        console.error(err);
+      }
+      setCompleted(true);
+    } else {
+      return <h2>You must enter all, no user was created</h2>;
     }
+  };
 
-return(
-<div>
-    {(!completed) &&
-    <section>
-      <h2 className = "subheader">Create an account</h2>
-      <form className = "inputs" onSubmit={onSubmitForm}>
-        <input
-          type="text"
-          placeholder="Username"
-          className="ex-input"
-          value={userName}
-          onChange={e => setUsername(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="email"
-          className="ex-input"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Password"
-          className="ex-input"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <button className = "btn-success">Add</button>
-      </form>
-    </section>
-    }
-        {(completed) && 
-        <div>
-             <h1>You are signed up and logged in</h1>
-             <h2>Your username is {userName}</h2>
-             <LoginPage username ={userName} email ={email} isUser = {true}/>
-        </div>}
-    </div>
-        )
+  const toLogin = () => {
+    window.location.href = "/login";
+  };
+
+  if (!completed) {
+    return (
+      <section>
+        <h2 className="subheader">Create an account</h2>
+        <form className="inputs" onSubmit={onSubmitForm}>
+          <input
+            type="text"
+            placeholder="Username"
+            className="ex-input"
+            value={userName}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email"
+            className="ex-input"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            className="ex-input"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button className="btn-success">Add</button>
+        </form>
+      </section>
+    );
+  } else {
+    return (
+      <div>
+        <h2>User Created</h2>
+        <button onClick={toLogin}>Proceed to the Log In Page</button>
+      </div>
+    );
+  }
 }
