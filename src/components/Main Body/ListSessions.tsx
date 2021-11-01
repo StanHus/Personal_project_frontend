4a049ffcf7af34993aefa0f445f79b009e4a4f40
StanHus <guseletovS@gmail.com>
2021-11-01 12:45:00 +0000

    listing by username done

--- src/components/Main Body/ListSessions.tsx
+++ src/components/Main Body/ListSessions.tsx
@@ -1,25 +1,31 @@
 import React, { Fragment, useEffect, useState } from "react";
 import EditSession from "./EditSession";
-import "../../css/style.css"
-
+import "../../css/style.css";
+import { useAuth } from "../authentification/AuthContext";
 
 interface ISession {
-  id: number,
-  muscles_trained: string
+  id: number;
+  muscles_trained: string;
+  user_email: string;
 }
 
 const ListSessions = () => {
   const [sessions, setSessions] = useState([]);
+  const { currentUser } = useAuth();
 
   const getSessions = async () => {
     try {
-      const response = await fetch("https://mysterious-reaches-13528.herokuapp.com/list");
+      const response = await fetch(
+        "https://mysterious-reaches-13528.herokuapp.com/list"
+      );
       const jsonData = await response.json();
       setSessions(jsonData);
     } catch (err) {
       console.error(err);
     }
   };
+
+  const checkUser = (user: any) => (user === null ? false : true);
 
   useEffect(() => {
     getSessions();
@@ -28,23 +34,46 @@ const ListSessions = () => {
   return (
     <Fragment>
       {" "}
-        <section className = "days">
-            <p className = "day"><strong>Mon</strong></p>
-            <p className = "day"><strong>Tue</strong></p>
-            <p className = "day"><strong>Wed</strong></p>
-            <p className = "day"><strong>Thu</strong></p>
-            <p className = "day"><strong>Fri</strong></p>
-            <p className = "day"><strong>Sat</strong></p>
-            <p className = "day"><strong>Sun</strong></p>
-        </section>
-      <table className = "list">
-        <tbody className = "containers">
-          {sessions.map((session: ISession) => (
-            <tr className = "entry" key={session.id}>
-               <td>
-                <EditSession session={session} />
-              </td>
-            </tr>))}
+      {checkUser(currentUser) && <h3>{currentUser.email}</h3>}
+      <section className="days">
+        <p className="day">
+          <strong>Mon</strong>
+        </p>
+        <p className="day">
+          <strong>Tue</strong>
+        </p>
+        <p className="day">
+          <strong>Wed</strong>
+        </p>
+        <p className="day">
+          <strong>Thu</strong>
+        </p>
+        <p className="day">
+          <strong>Fri</strong>
+        </p>
+        <p className="day">
+          <strong>Sat</strong>
+        </p>
+        <p className="day">
+          <strong>Sun</strong>
+        </p>
+      </section>
+      <table className="list">
+        <tbody className="containers">
+          {sessions
+            // eslint-disable-next-line
+            .filter((session: ISession) => {
+              if (checkUser(currentUser)) {
+                return session.user_email === currentUser.email;
+              }
+            })
+            .map((session: ISession) => (
+              <tr className="entry" key={session.id}>
+                <td>
+                  <EditSession session={session} />
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </Fragment>
